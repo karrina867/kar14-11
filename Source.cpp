d92b313ad6bc7150791e806f759aad3464e2c6ea
karrina867 <92594854+karrina867@users.noreply.github.com>
2021-11-14 18:03:18 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+//Karina Baslyk 14.11
+#include<iostream>
+using namespace std;
+int main() {
+	//2. напечатать все числа в диапозоне от a до b с шагом k.
+	/*int a,b,k;
+	cin >> a >> b >> k;
+	for (int i = a; i <= b; i=i+k) { cout << i << ' ';}
+	cout << endl;*/
+	//1. 
+	for (int i = 5; i <= 95; i = i + 5) { cout << i << ' ';}
+	cout << endl;
+	return 0;
+}
